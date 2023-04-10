173e2fc22a993644204a383c8ce9328baed98c23
drookey <arpitgrwl9@gmail.com>
2023-04-09 18:12:20 -0700

    Razorpay Clone
    Razorpay Clone Project

---
+++ Razorpay Clone/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042A",
+        lightBlue:"#2B84EA",
+        lightBlu300:"#4B94ED",
+        lightBlue500:"#0B72E7",
+        greenLight:"#61CEA6",
+        grayText:"#818597",
+        lightGray:"#E2E2E2",
+        grayBlue:"#344A6C",
+        deepBlueHead:"#162F56",
+        gray2:"#525A76"
+      }
+    },
+  },
+  plugins: [],
+}
